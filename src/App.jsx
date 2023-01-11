35086d097e3259fc4225b29677dfa7311ccb19ed
Diouani <diouanijalouka@gmail.com>
2023-01-11 13:44:50 +0100

    create  Component Folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Register from './Component/User/Register/Register';
 
 function App() {
   return (
     <div className="App">
-    
+    <Register/>
     </div>
   );
 }
