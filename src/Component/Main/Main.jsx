717caf32500a234005cb658c362b88ff673bdb7e
Diouani <diouanijalouka@gmail.com>
2023-01-12 11:44:41 +0100

    adding a MainHeader file

--- src/Component/Main/Main.jsx
+++ src/Component/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react'
 import HeadingPage from '../HeadingPage/HeadingPage'
+import MainHeader from '../MainHeader/MainHeader'
 
 
 const Main = () => {
     
   return (
     <div>
+      <MainHeader/>
       <HeadingPage/>
     </div>
   )
