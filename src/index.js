68dfffd4effc1adbd4bc87d2fe73090b44957ea6
Vijaya Challa <vijayajay1814@gmail.com>
2023-01-12 11:20:48 +0100

    modified Headingpage

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import DataOfUser from "./DataOfUser";
 import ImageFileContextProvider from "./context/ImageFileContext";
+import PersonalDetailsContextProvider from "./context/PersonalDetailsContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <DataOfUser>
     <ImageFileContextProvider>
+    <PersonalDetailsContextProvider>
       <App />
+      </PersonalDetailsContextProvider>
     </ImageFileContextProvider>
   </DataOfUser>
 );
