8e4057b22203ef64bf3a7ee5fb36770028acc7ce
Diouani <diouanijalouka@gmail.com>
2023-01-16 15:30:58 +0100

    nothing to comment

--- src/Component/Education/Education.jsx
+++ src/Component/Education/Education.jsx
@@ -6,6 +6,7 @@ import { NavLink } from "react-router-dom";
 
 const Education = () => {
   const { edu, efo } = useContext(EduSkillContext);
+
   return (
     <div className="education">
       <h1>
